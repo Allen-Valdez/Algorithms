e4a2d6929a9df1bd095ddb222b4ef5cf0f0ad91a
Allen-Valdez <68507863+Allen-Valdez@users.noreply.github.com>
2021-03-22 22:42:08 -0700

    Fourth commit

---
+++ Factorial.js
@@ -0,0 +1,19 @@
+// For loop
+// const factorial = num => {
+//   total = 1;
+//   for (let i = num; i > 1; i--) {
+//     total *= i;
+//   }
+//   return total;
+// };
+
+// console.log(factorial(4));
+
+// Recursion
+
+const factorial2 = num => {
+  if (num === 1) return 1;
+  return num * factorial2(num - 1);
+};
+
+console.log(factorial2(4))
---
+++ Recursion.js
@@ -0,0 +1,11 @@
+const countDown = num => {
+  if (num <= 0) {
+    console.log('All Down!');
+    return;
+  }
+  console.log(num);
+  num--;
+  countDown(num);
+};
+
+countDown(5); 
